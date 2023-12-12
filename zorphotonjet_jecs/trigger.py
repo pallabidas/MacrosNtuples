2044abc61e35ffdbc718bf113d527d0d01cbf43c
gparaske <gianneios.paraskevas@cern.ch>
2023-12-12 13:44:09 +0100

    Comment out printing (only for testing)

--- zorphotonjet_jecs/trigger.py
+++ zorphotonjet_jecs/trigger.py
@@ -12,7 +12,7 @@ def TriggerInit(df, channel):
  
     if channel == 'Photon':
        triggers = fnmatch.filter(columns, 'HLT_Photon*EB_TightID_TightIso') 
-       print('Trigger list : ',triggers)  
+       #print('Trigger list : ',triggers)  
   
     return triggers
 
@@ -23,6 +23,7 @@ def TriggerFired(triggers):
    trigger_fired = triggers[0]
    for t in triggers[1:]:
        trigger_fired += ' || ' + t
+
    return trigger_fired 
 
 
@@ -31,16 +32,16 @@ def TriggerSelect(triggers, channel = 'Photon'):
  
     # Create a list with all pt thresholds
     thresholds = [int(re.findall(r'\d+', t)[0]) for t in triggers if re.findall(r'\d+', t)]
-    print('Trigger thresholds : ',thresholds)
+    #print('Trigger thresholds : ',thresholds)
     
     # We use a trigger considering 5 GeV to become efficient
     thresholds = list(map(lambda i: i + 5, thresholds))
-    print('Trigger thresholds considering 5 GeV for efficiency: ',thresholds)
+    #print('Trigger thresholds considering 5 GeV for efficiency: ',thresholds)
 
     expr = '('
     for i in range(len(thresholds)-1):
         expr += '(' + channel + '_pt>' + str(thresholds[i]) + '&&' + channel + '_pt<=' + str(thresholds[i+1]) + '&&' + triggers[i] + ')||'
     expr += '(' + channel + '_pt>' + str(thresholds[-1]) + '&&' + triggers[-1] + '))'
-    print('Expression for filtering events according to trigger information: ',expr)
+    #print('Expression for filtering events according to trigger information: ',expr)
 
     return expr
